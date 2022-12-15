d06dcb1beb8cf1ec843fd2a6ebc70f35cb559aad
laggykiller <chaudominic2@gmail.com>
2022-12-16 02:28:31 +0800

    add scripts for auto getting dependencies

--- get-deps-macos.sh
+++ get-deps-macos.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 # https://github.com/ImageMagick/ImageMagick/discussions/5215#discussioncomment-2949450
 
-# if uname -s | grep -q "Linux" ; then
-#     if cat /etc/issue | grep -q "Ubuntu"; then
-#         apt update 
-#         apt -y install curl zip gcc cmake make zlib1g-dev g++ 
-#     fi
-# fi
-
+# Compile ImageMagick
 cd ./sticker_convert
 export MAGICK_HOME=$(pwd)/ImageMagick
 cd ..
@@ -50,3 +44,54 @@ rm config.status config.log
 make clean
 make
 make install
+
+cd ../../
+rm -rf ./magick-src
+
+# Preparing bin directory
+rm -rf ./sticker_convert/bin
+mkdir ./sticker_convert/bin
+cd ./sticker_convert/bin
+
+# Compiling pngnq-s9
+mkdir ./pngnqs9
+cd ./pngnqs9
+curl -O -L https://github.com/ImageProcessing-ElectronicPublications/pngnq-s9/archive/refs/tags/2.0.2.tar.gz
+tar xvzf 2.0.2.tar.gz
+cd ./pngnq-s9-2.0.2
+# Fix bug that causes compilation to fail
+echo '#include <string.h>' > src/rwpng.c.new
+cat src/rwpng.c >> src/rwpng.c.new
+mv src/rwpng.c.new src/rwpng.c
+./configure
+make
+# make install
+chmod +x src/pngnq-s9
+cd ../../
+cp ./pngnqs9/pngnq-s9-2.0.2/src/pngnq-s9 ./
+rm -rf ./pngnqs9
+
+# Get apngasm
+curl -O -L https://sourceforge.net/projects/apngasm/files/2.91/apngasm-2.91-bin-macos.zip
+unzip apngasm-2.91-bin-macos.zip
+rm readme.txt
+rm apngasm-2.91-bin-macos.zip
+
+# Get apngdis
+curl -O -L https://sourceforge.net/projects/apngdis/files/2.9/apngdis-2.9-bin-macos.zip
+unzip apngdis-2.9-bin-macos.zip
+rm readme.txt
+rm apngdis-2.9-bin-macos.zip
+
+# Get ffmpeg
+curl -O -L https://evermeet.cx/ffmpeg/ffmpeg-5.1.2.zip
+unzip ffmpeg-5.1.2.zip
+rm ffmpeg-5.1.2.zip
+
+# Get ffprobe
+curl -O -L https://evermeet.cx/ffmpeg/ffprobe-5.1.2.zip
+unzip ffprobe-5.1.2.zip
+rm ffprobe-5.1.2.zip
+
+# Go back to root of repo
+cd ../../
