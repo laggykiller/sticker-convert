da82130ef4f2bfa294a22f0ad1df1c5fbef20a8d
laggykiller <chaudominic2@gmail.com>
2022-12-16 18:58:15 +0800

    Fix missing binaries during auto build

--- get-deps-windows.bat
+++ get-deps-windows.bat
@@ -25,6 +25,27 @@ curl -O -L "https://sourceforge.net/projects/apngdis/files/2.8/apngdis-2.8-bin-w
 tar -xf apngdis-2.8-bin-win32.zip
 del apngdis-2.8-bin-win32.zip
 
+:: Get pngnq-s9
+curl -O -L "https://sourceforge.net/projects/pngnqs9/files/pngnq-s9-2.0.1-win32.zip"
+tar -xf pngnq-s9-2.0.1-win32.zip
+move pngnq-s9-2.0.1-win32\pngnq-s9-2.0.1-win32.exe .\pngnq-s9.exe
+del pngnq-s9-2.0.1-win32.zip
+rmdir -r pngnq-s9-2.0.1-win32
+
+:: Get optipng
+curl -O -L "https://sourceforge.net/projects/optipng/files/OptiPNG/optipng-0.7.7/optipng-0.7.7-win32.zip"
+tar -xf optipng-0.7.7-win32.zip
+move optipng-0.7.7-win32\optipng.exe .
+del optipng-0.7.7-win32.zip
+rmdir -r optipng-0.7.7-win32
+
+:: Get pngquant
+curl -O -L "https://pngquant.org/pngquant-windows.zip"
+tar -xf pngquant-windows.zip
+move pngquant\pngquant.exe .
+del pngquant-windows.zip
+rmdir -r pngquant
+
 :: Get ffmpeg
 curl -O -L "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip"
 tar -xf ffmpeg-master-latest-win64-gpl.zip
