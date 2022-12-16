1ab3f7c5e755e85f68938742cb7303ff182f9d6d
laggykiller <chaudominic2@gmail.com>
2022-12-16 21:20:26 +0800

    fix build downloading resources

--- get-deps-windows.bat
+++ get-deps-windows.bat
@@ -9,32 +9,32 @@ rd /s /q sticker_convert\ImageMagick >nul 2>&1
 :: Get ImageMagick
 mkdir ImageMagick
 cd .\ImageMagick
-curl --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -O -L "https://imagemagick.org/archive/binaries/ImageMagick-7.1.0-54-portable-Q16-x64.zip"
+curl --retry 5 -O -L "https://imagemagick.org/archive/binaries/ImageMagick-7.1.0-54-portable-Q16-x64.zip"
 tar -xf ImageMagick-7.1.0-54-portable-Q16-x64.zip
 del ImageMagick-7.1.0-54-portable-Q16-x64.zip
 cd ..
 
 :: Get apngasm
-curl --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -O -L "https://github.com/laggykiller/apngasm/releases/download/3.1.3/apngasm_3.1-3_AMD64.zip"
+curl --retry 5 -O -L "https://github.com/laggykiller/apngasm/releases/download/3.1.3/apngasm_3.1-3_AMD64.zip"
 tar -xf apngasm_3.1-3_AMD64.zip
 del apngasm_3.1-3_AMD64.zip
 
 :: Get apngdis
 cd bin
-curl --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -O -L "https://sourceforge.net/projects/apngdis/files/2.9/apngdis-2.9-bin-win64.zip"
+curl --retry 5 -O -L "https://sourceforge.net/projects/apngdis/files/2.9/apngdis-2.9-bin-win64.zip"
 tar -xf apngdis-2.9-bin-win64.zip
 del apngdis-2.9-bin-win64.zip
 del readme.txt
 
 :: Get pngnq-s9
-curl --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -O -L "https://sourceforge.net/projects/pngnqs9/files/pngnq-s9-2.0.2.zip"
+curl --retry 5 -O -L "https://sourceforge.net/projects/pngnqs9/files/pngnq-s9-2.0.2.zip"
 tar -xf pngnq-s9-2.0.2.zip
 move pngnq-s9-2.0.2\pngnq-s9.exe .
 del pngnq-s9-2.0.2.zip
 rd /s /q pngnq-s9-2.0.2
 
 :: Get optipng
-curl --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -O -L "https://sourceforge.net/projects/optipng/files/OptiPNG/optipng-0.7.7/optipng-0.7.7-win32.zip"
+curl --retry 5 -O -L "https://sourceforge.net/projects/optipng/files/OptiPNG/optipng-0.7.7/optipng-0.7.7-win32.zip"
 tar -xf optipng-0.7.7-win32.zip
 move optipng-0.7.7-win32\optipng.exe .
 del optipng-0.7.7-win32.zip
@@ -43,7 +43,7 @@ rd /s /q optipng-0.7.7-win32
 :: Get pngquant
 mkdir pngquant-dl
 cd pngquant-dl
-curl --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -O -L "https://pngquant.org/pngquant-windows.zip"
+curl --retry 5 -O -L "https://pngquant.org/pngquant-windows.zip"
 tar -xf pngquant-windows.zip
 cd ..
 move pngquant-dl\pngquant\pngquant.exe .
@@ -52,7 +52,7 @@ rd /s /q pngquant-dl
 :: Get ffmpeg
 mkdir ffmpeg-dl
 cd ffmpeg-dl
-curl --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -O -L "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip"
+curl --retry 5 -O -L "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip"
 tar -xf ffmpeg-master-latest-win64-gpl.zip
 cd ..
 move ffmpeg-dl\ffmpeg-master-latest-win64-gpl\bin\ffmpeg.exe .
@@ -62,7 +62,7 @@ rd /s /q ffmpeg-dl
 :: Get bzip2
 mkdir bzip2-dl
 cd bzip2-dl
-curl --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -O -L "https://sourceforge.net/projects/gnuwin32/files/bzip2/1.0.5/bzip2-1.0.5-bin.zip"
+curl --retry 5 -O -L "https://sourceforge.net/projects/gnuwin32/files/bzip2/1.0.5/bzip2-1.0.5-bin.zip"
 tar -xf bzip2-1.0.5-bin.zip
 cd ..
 move bzip2-dl\bin\* .
@@ -71,7 +71,7 @@ rd /s /q bzip2-dl
 :: Get zip
 mkdir zip-dl
 cd zip-dl
-curl --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -O -L "http://downloads.sourceforge.net/gnuwin32/zip-3.0-bin.zip"
+curl --retry 5 -O -L "http://downloads.sourceforge.net/gnuwin32/zip-3.0-bin.zip"
 tar -xf zip-3.0-bin.zip
 cd ..
 move zip-dl\bin\* .
