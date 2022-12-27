ed361322f8f3bea6e29fda5076e6e66fa35a07df
laggykiller <chaudominic2@gmail.com>
2022-12-27 22:40:07 +0800

    debugging build with cairo dll

--- get-deps-windows.bat
+++ get-deps-windows.bat
@@ -11,6 +11,8 @@ curl --retry 5 -O -L "https://downloads.sk1project.net/uc2/MS_Windows/uniconvert
 start /wait msiexec /qn /i uniconvertor-2.0rc5-win64_headless.msi
 del uniconvertor-2.0rc5-win64_headless.msi
 powershell -command "[Environment]::SetEnvironmentVariable('Path', $env:Path + ';C:\Program Files\UniConvertor-2.0rc5\dlls', 'Machine')"
+dir C:\Program Files\UniConvertor-2.0rc5\dlls
+echo %PATH%
 
 :: Get apngasm
 curl --retry 5 -O -L "https://github.com/laggykiller/apngasm/releases/download/3.1.3/apngasm_3.1-3_AMD64.zip"
