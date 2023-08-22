f78ce4a40597a79b2e1a69333e884aba44561df7
laggykiller <61652821+laggykiller@users.noreply.github.com>
2023-08-23 01:41:54 +0800

    prevent creating files if installed, include pip

--- src/sticker_convert/utils/curr_dir.py
+++ src/sticker_convert/utils/curr_dir.py
@@ -7,45 +7,62 @@ class CurrDir:
     def get_curr_dir():
         appimage_path = os.getenv('APPIMAGE')
 
-        curr_dir = ''
+        cwd = os.getcwd()
+        if os.path.isdir(os.path.expanduser('~/Desktop')):
+            fallback_dir = os.path.expanduser('~/Desktop')
+        else:
+            fallback_dir = os.path.expanduser('~')
 
-        if sys.platform == 'darwin' and getattr(sys, 'frozen', False) and not os.getcwd().startswith('/Applications/') and '.app/Contents/MacOS' in os.getcwd():
-            curr_dir = '../../../'
+        if sys.platform == 'darwin' and getattr(sys, 'frozen', False) and '.app/Contents/MacOS' in cwd:
+            if cwd.startswith('/Applications/'):
+                curr_dir = fallback_dir
+            else:
+                curr_dir = '../../../'
         elif appimage_path:
             curr_dir = os.path.split(appimage_path)[0]
-        
-        curr_dir = os.path.abspath(curr_dir)
+        elif (cwd.startswith('/usr/bin/') 
+              or cwd.startswith('/bin') 
+              or cwd.startswith('/usr/local/bin')
+              or cwd.startswith('C:\\Program Files')
+              or 'site-packages' in __file__):
+            curr_dir = fallback_dir
+        else:
+            curr_dir = cwd
         
         if os.access(curr_dir, os.W_OK):
             return curr_dir
-        
-        if os.path.isdir(os.path.expanduser('~/Desktop')):
-            curr_dir = os.path.expanduser('~/Desktop')
         else:
-            curr_dir = os.path.expanduser('~')
-        
-        return curr_dir
+            return fallback_dir
     
     @staticmethod
     def get_creds_dir():
         appimage_path = os.getenv('APPIMAGE')
 
-        creds_dir = ''
+        cwd = os.getcwd()
+        if sys.platform == 'win32':
+            fallback_dir = os.path.expandvars('%APPDATA%/sticker-convert')
+        else:
+            fallback_dir = os.path.expanduser('~/.config/sticker-convert')
 
-        if sys.platform == 'darwin' and getattr(sys, 'frozen', False) and not os.getcwd().startswith('/Applications/') and '.app/Contents/MacOS' in os.getcwd():
-            creds_dir = '../../../'
+        if sys.platform == 'darwin' and getattr(sys, 'frozen', False) and '.app/Contents/MacOS' in cwd:
+            if cwd.startswith('/Applications/'):
+                creds_dir = fallback_dir
+            else:
+                creds_dir = '../../../'
         elif appimage_path:
             creds_dir = os.path.split(appimage_path)[0]
-        
-        creds_dir = os.path.abspath(creds_dir)
-
-        if os.access(creds_dir, os.W_OK):
-            return creds_dir
-        
-        if sys.platform == 'win32':
-            creds_dir = os.path.expandvars('%APPDATA%/sticker-convert')
+        elif (cwd.startswith('/usr/bin/') 
+              or cwd.startswith('/bin') 
+              or cwd.startswith('/usr/local/bin')
+              or cwd.startswith('C:\\Program Files')
+              or 'site-packages' in __file__):
+            creds_dir = fallback_dir
         else:
-            creds_dir = os.path.expanduser('~/.config/sticker-convert')
+            creds_dir = cwd
         
         os.makedirs(creds_dir, exist_ok=True)
-        return creds_dir
+        if os.access(creds_dir, os.W_OK):
+            return creds_dir
+        else:
+            os.makedirs(fallback_dir, exist_ok=True)
+            return fallback_dir
