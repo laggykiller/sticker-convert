ea4be1b7adb24af13db67928dc43960e2cc4bf37
laggykiller <61652821+laggykiller@users.noreply.github.com>
2023-10-02 03:53:22 +0800

    Cleanup compile.py

--- compile.py
+++ compile.py
@@ -60,113 +60,93 @@ def create_universal_wheels(in_dir1, in_dir2, out_dir):
         os.rename(src_path, dst_path)
         print(f'Renamed universal wheel {dst_path}')
 
-arch = os.environ.get('SC_COMPILE_ARCH')
+def osx_install_universal2_dep():
+    shutil.rmtree('wheel_arm', ignore_errors=True)
+    shutil.rmtree('wheel_x64', ignore_errors=True)
+    shutil.rmtree('wheel_universal2', ignore_errors=True)
 
-if platform.system() == 'Windows':
-    use_shell = True
-else:
-    use_shell = False
+    os.mkdir('wheel_arm')
+    os.mkdir('wheel_x64')
+    os.mkdir('wheel_universal2')
 
-python_bin = os.path.abspath(sys.executable)
+    osx_run_in_venv('python -m pip install -r requirements-src.txt')
+    osx_run_in_venv('python -m pip download --require-virtualenv -r requirements-bin.txt --platform macosx_11_0_arm64 --only-binary=:all: -d wheel_arm')
+    osx_run_in_venv('python -m pip download --require-virtualenv -r requirements-bin.txt --platform macosx_11_0_x86_64 --only-binary=:all: -d wheel_x64')
 
-shutil.make_archive('src/sticker_convert/ios-message-stickers-template', 'zip', 'src/sticker_convert/ios-message-stickers-template')
-shutil.rmtree('venv', ignore_errors=True)
+    create_universal_wheels('./wheel_arm', './wheel_x64', 'wheel_universal2')
+    osx_run_in_venv(f'python -m pip install --require-virtualenv ./wheel_universal2/*')
 
-if platform.system() == 'Darwin':
-    subprocess.run(f'{python_bin} -m pip install --upgrade pip delocate'.split(' '))
-    subprocess.run(f'{python_bin} -m venv venv'.split(' '))
-    python_bin = 'python'
-    osx_run_in_venv('python -m pip install wheel nuitka')
+def osx_create_fake_universal2_dylib():
+    osx_run_in_venv('clang -arch arm64 -shared -undefined dynamic_lookup -o ./scripts/stub-arm64.dylib ./scripts/stub.c')
+    osx_run_in_venv('clang -arch x86_64 -shared -undefined dynamic_lookup -o ./scripts/stub-x86_64.dylib ./scripts/stub.c')
 
-    if not arch:
-        osx_run_in_venv('python -m pip install --require-virtualenv -r requirements-build.txt')
+    stub_x64 = './scripts/stub-x86_64.dylib'
+    stub_arm = './scripts/stub-arm64.dylib'
+    
+    for dirpath, dirnames, filenames in os.walk('venv/lib'):
+        for f in filenames:
+            if os.path.splitext(f)[1] == '.dylib':
+                f_path = os.path.join(dirpath, f)
+                out = osx_run_in_venv(f'lipo -info {f_path}', get_stdout=True)
+                if f_path.endswith('-x86_64.dylib') or f_path.endswith('-arm64.dylib'):
+                    continue
+                if 'x86_64' in out and 'arm64' in out:
+                    continue
+                if 'x86_64' in out:
+                    f_bak = os.path.splitext(f)[0] + '-x86_64.dylib'
+                    f_bak_path = os.path.join(dirpath, f_bak)
+                    os.rename(f_path, f_bak_path)
+                    osx_run_in_venv(f'lipo {f_bak_path} {stub_arm} -create -output {f_path}', get_stdout=True)
+                    print(f'Created fat library {f}')
+                elif 'arm64' in out:
+                    f_bak = os.path.splitext(f)[0] + '-arm64.dylib'
+                    f_bak_path = os.path.join(dirpath, f_bak)
+                    os.rename(f_path, f_bak_path)
+                    osx_run_in_venv(f'lipo {f_bak_path} {stub_x64} -create -output {f_path}', get_stdout=True)
+                    print(f'Created fat library {f}')
+
+def nuitka(python_bin, arch):
+    cmd_list = [
+        python_bin,
+        '-m',
+        'nuitka',
+        '--standalone',
+        '--follow-imports',
+        '--assume-yes-for-downloads',
+        '--include-data-files=src/sticker_convert/ios-message-stickers-template.zip=ios-message-stickers-template.zip',
+        '--include-data-dir=src/sticker_convert/resources=resources',
+        '--enable-plugin=tk-inter',
+        '--enable-plugin=multiprocessing',
+        '--include-package-data=signalstickers_client',
+        '--include-package=av',
+        '--include-module=av.audio.codeccontext',
+        '--include-module=av.video.codeccontext',
+        '--include-package=imageio',
+        '--noinclude-data-file=tcl/opt0.4',
+        '--noinclude-data-file=tcl/http1.0',
+        '--user-package-configuration-file=nuitka.config.yml',
+        '--macos-create-app-bundle',
+        '--macos-app-icon=src/sticker_convert/resources/appicon.icns',
+    ]
+
+    if platform.system() == 'Windows':
+        cmd_list.append('--windows-icon-from-ico=src/sticker_convert/resources/appicon.ico')
+    elif platform.system() == 'Darwin' and arch:
+        cmd_list.append(f'--macos-target-arch={arch}')
+
+    cmd_list.append('src/sticker-convert.py')
+    if platform.system() == 'Darwin':
+        osx_run_in_venv(' '.join(cmd_list))
     else:
-        shutil.rmtree('wheel_arm', ignore_errors=True)
-        shutil.rmtree('wheel_x64', ignore_errors=True)
-        shutil.rmtree('wheel_universal2', ignore_errors=True)
+        subprocess.run(cmd_list, shell=True)
 
-        os.mkdir('wheel_arm')
-        os.mkdir('wheel_x64')
-        os.mkdir('wheel_universal2')
-
-        osx_run_in_venv('python -m pip install -r requirements-src.txt')
-        osx_run_in_venv('python -m pip download --require-virtualenv -r requirements-bin.txt --platform macosx_11_0_arm64 --only-binary=:all: -d wheel_arm')
-        osx_run_in_venv('python -m pip download --require-virtualenv -r requirements-bin.txt --platform macosx_11_0_x86_64 --only-binary=:all: -d wheel_x64')
-
-        osx_run_in_venv('clang -arch arm64 -shared -undefined dynamic_lookup -o ./scripts/stub-arm64.dylib ./scripts/stub.c')
-        osx_run_in_venv('clang -arch x86_64 -shared -undefined dynamic_lookup -o ./scripts/stub-x86_64.dylib ./scripts/stub.c')
-
-        stub_x64 = './scripts/stub-x86_64.dylib'
-        stub_arm = './scripts/stub-arm64.dylib'
-    
-        create_universal_wheels('./wheel_arm', './wheel_x64', 'wheel_universal2')
-        osx_run_in_venv(f'python -m pip install --require-virtualenv ./wheel_universal2/*')
-    
-        for dirpath, dirnames, filenames in os.walk('venv/lib'):
-            for f in filenames:
-                if os.path.splitext(f)[1] == '.dylib':
-                    f_path = os.path.join(dirpath, f)
-                    out = osx_run_in_venv(f'lipo -info {f_path}', get_stdout=True)
-                    if f_path.endswith('-x86_64.dylib') or f_path.endswith('-arm64.dylib'):
-                        continue
-                    if 'x86_64' in out and 'arm64' in out:
-                        continue
-                    if 'x86_64' in out:
-                        f_bak = os.path.splitext(f)[0] + '-x86_64.dylib'
-                        f_bak_path = os.path.join(dirpath, f_bak)
-                        os.rename(f_path, f_bak_path)
-                        osx_run_in_venv(f'lipo {f_bak_path} {stub_arm} -create -output {f_path}', get_stdout=True)
-                        print(f'Created fat library {f}')
-                    elif 'arm64' in out:
-                        f_bak = os.path.splitext(f)[0] + '-arm64.dylib'
-                        f_bak_path = os.path.join(dirpath, f_bak)
-                        os.rename(f_path, f_bak_path)
-                        osx_run_in_venv(f'lipo {f_bak_path} {stub_x64} -create -output {f_path}', get_stdout=True)
-                        print(f'Created fat library {f}')
-elif platform.system() == 'Windows':
-    subprocess.run(f'{python_bin} -m pip install --upgrade pip'.split(' '), shell=use_shell)
-    subprocess.run(f'{python_bin} -m pip install -r requirements-build.txt'.split(' '), shell=use_shell)
-
-cmd_list = [
-    python_bin,
-    '-m',
-    'nuitka',
-    '--standalone',
-    '--follow-imports',
-    '--assume-yes-for-downloads',
-    '--include-data-files=src/sticker_convert/ios-message-stickers-template.zip=ios-message-stickers-template.zip',
-    '--include-data-dir=src/sticker_convert/resources=resources',
-    '--enable-plugin=tk-inter',
-    '--enable-plugin=multiprocessing',
-    '--include-package-data=signalstickers_client',
-    '--include-package=av',
-    '--include-module=av.audio.codeccontext',
-    '--include-module=av.video.codeccontext',
-    '--include-package=imageio',
-    '--noinclude-data-file=tcl/opt0.4',
-    '--noinclude-data-file=tcl/http1.0',
-    '--user-package-configuration-file=nuitka.config.yml',
-    '--macos-create-app-bundle',
-    '--macos-app-icon=src/sticker_convert/resources/appicon.icns',
-]
-
-if platform.system() == 'Windows':
-    cmd_list.append('--windows-icon-from-ico=src/sticker_convert/resources/appicon.ico')
-elif platform.system() == 'Darwin' and arch:
-    cmd_list.append(f'--macos-target-arch={arch}')
-
-cmd_list.append('src/sticker-convert.py')
-if platform.system() == 'Darwin':
-    osx_run_in_venv(' '.join(cmd_list))
-else:
-    subprocess.run(cmd_list, shell=use_shell)
-
-if platform.system() == 'Windows':
+def win_patch():
     for i in os.listdir('sticker-convert.dist/av.libs'):
         file_path = os.path.join('sticker-convert.dist', i)
         if os.path.isfile(file_path):
             os.remove(file_path)
-elif platform.system() == 'Darwin' and arch:
+
+def osx_patch():
     # https://github.com/Nuitka/Nuitka/issues/1511#issuecomment-1113260273
     site = osx_run_in_venv("python -c 'import site; print(site.getsitepackages()[0])'", get_stdout=True).strip()
     pil_dylibs = os.path.join(site, 'PIL/.dylibs')
@@ -177,4 +157,39 @@ elif platform.system() == 'Darwin' and arch:
     os.makedirs('sticker-convert.app/Contents/MacOS/PIL/.dylibs', exist_ok=True)
     shutil.copy(libjpeg, 'sticker-convert.app/Contents/MacOS')
     shutil.copy(libjpeg, 'sticker-convert.app/Contents/MacOS/PIL/.dylibs')
-    subprocess.run(f'codesign --force --deep -s - sticker-convert.app'.split(' '), stdout=subprocess.PIPE).stdout.decode()
+    osx_run_in_venv(f'codesign --force --deep -s - sticker-convert.app')
+
+def compile():
+    arch = os.environ.get('SC_COMPILE_ARCH')
+    python_bin = os.path.abspath(sys.executable)
+
+    ios_stickers_path = 'src/sticker_convert/ios-message-stickers-template'
+    ios_stickers_zip = ios_stickers_path + '.zip'
+    if os.path.isfile(ios_stickers_zip):
+        os.remove(ios_stickers_zip)
+    shutil.make_archive(ios_stickers_path, 'zip', ios_stickers_path)
+
+    if platform.system() == 'Windows':
+        subprocess.run(f'{python_bin} -m pip install --upgrade pip'.split(' '), shell=True)
+        subprocess.run(f'{python_bin} -m pip install -r requirements-build.txt'.split(' '), shell=True)
+    elif platform.system() == 'Darwin':
+        shutil.rmtree('venv', ignore_errors=True)
+        subprocess.run(f'{python_bin} -m pip install --upgrade pip delocate'.split(' '))
+        subprocess.run(f'{python_bin} -m venv venv'.split(' '))
+        python_bin = 'python'
+        osx_run_in_venv('python -m pip install wheel nuitka')
+        if not arch:
+            osx_run_in_venv('python -m pip install --require-virtualenv -r requirements-build.txt')
+        else:
+            osx_install_universal2_dep()
+            osx_create_fake_universal2_dylib()
+
+    nuitka(python_bin, arch)
+
+    if platform.system() == 'Windows':
+        win_patch()
+    elif platform.system() == 'Darwin' and arch:
+        osx_patch()
+
+if __name__ == '__main__':
+    compile()
