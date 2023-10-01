bc61fc64f373ffe9888a345e5005b07ce51c76cf
laggykiller <61652821+laggykiller@users.noreply.github.com>
2023-10-01 08:02:54 +0800

    Fix unable to create xcode project

--- src/sticker_convert/utils/converter.py
+++ src/sticker_convert/utils/converter.py
@@ -84,7 +84,10 @@ class StickerConvert:
         self.duration_min = opt_comp.get('duration', {}).get('min') if not self.duration else self.duration
         self.duration_max = opt_comp.get('duration', {}).get('max') if not self.duration else self.duration
 
-        self.steps = opt_comp.get('steps') if self.size_max == None else 1
+        if not self.size_max and not self.size_max_img and not self.size_max_vid:
+            self.steps = 1
+        else:
+            self.steps = opt_comp.get('steps') if opt_comp.get('steps') else 1
         self.fake_vid = opt_comp.get('fake_vid')
         self.cache_dir = opt_comp.get('cache_dir')
 
