33f64e4f36788ecb5b064d2331566985758647bd
laggykiller <chaudominic2@gmail.com>
2024-01-02 14:50:58 +0800

    Print version at start

--- src/sticker_convert/__main__.py
+++ src/sticker_convert/__main__.py
@@ -3,8 +3,10 @@ def main():
     import multiprocessing
     import sys
     import os
+    from .__init__ import __version__
 
     multiprocessing.freeze_support()
+    print(f"sticker-convert {__version__}")
     script_path = os.path.dirname(__file__)
     if not os.path.isdir(script_path):
         script_path = os.path.dirname(sys.argv[0])
