d66195305ca325dd208a0ec3661643aba6e5590c
laggykiller <chaudominic2@gmail.com>
2024-08-11 02:53:42 +0800

    Move commands for building appimage to shell script

---
+++ scripts/build_appimage.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+if [ -z ${SC_COMPILE_ARCH} ]; then
+    echo "Please set env variable 'SC_COMPILE_ARCH' (Supported: x86_64, aarch64)"
+    exit
+fi
+
+sudo apt install -y libpng-dev libxft-dev libfontconfig1-dev libfreetype6-dev
+
+wget -O appimage-builder https://github.com/AppImageCrafters/appimage-builder/releases/download/v1.1.0/appimage-builder-1.1.0-x86_64.AppImage
+wget -O appimagetool https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-x86_64.AppImage
+chmod +x ./appimage-builder
+chmod +x ./appimagetool
+
+# Creating appimage directly with appimage-builder result in xz compression
+# That would be not possible to be verified by https://appimage.github.io/apps/
+# Due to https://github.com/AppImage/appimage.github.io/blob/master/code/worker.sh
+# runtime-fuse2-x86_64 unable to handle xz compression
+./appimage-builder --skip-appimage --recipe AppImageBuilder-x86_64.yml
+
+# Add .desktop comment
+sed -i 's/Comment=/Comment=Convert (animated) stickers between WhatsApp, Telegram, Signal, Line, Kakao, Viber, Discord, iMessage/g' ./AppDir/*.desktop
+
+# Add appdata.xml
+mkdir -p AppDir/usr/share/metainfo
+cp ./sticker-convert.appdata.xml AppDir/usr/share/metainfo
+
+# Bundling into appimage
+wget https://github.com/AppImage/AppImageKit/releases/download/13/runtime-${SC_COMPILE_ARCH}
+if [ ${SC_COMPILE_ARCH} == "aarch64" ]; then
+    ARCH="arm_aarch64"
+else
+    ARCH=${SC_COMPILE_ARCH}
+fi
+./appimagetool --runtime-file ./runtime-${SC_COMPILE_ARCH} ./AppDir
+chmod +x sticker-convert-${SC_COMPILE_ARCH}.AppImage
