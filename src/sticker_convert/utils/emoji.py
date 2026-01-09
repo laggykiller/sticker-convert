5524bfb5f03bb5370b56288b46b63f134a3fd620
laggykiller <chaudominic2@gmail.com>
2026-01-09 21:33:35 +0800

    Fix handling emojis with modifiers

--- src/sticker_convert/utils/emoji.py
+++ src/sticker_convert/utils/emoji.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
-from typing import List
+from typing import Callable, List, Sequence, Set
+
+from ugrapheme import graphemes  # type: ignore
 
 from sticker_convert.utils.files.json_resources_loader import load_resource_json
+
+graphemes: Callable[[str], Sequence[str]]  # type: ignore
+
+
+# https://stackoverflow.com/a/480227
+# Return list of unique items of list while preserve order
+def uniques(seq: List[str]):
+    seen: Set[str] = set()
+    seen_add = seen.add
+    return [x for x in seq if not (x in seen or seen_add(x))]
 
 
 def get_emoji_list() -> List[str]:
@@ -13,4 +25,4 @@ EMOJI_LIST = get_emoji_list()
 
 # https://stackoverflow.com/a/43146653
 def extract_emojis(s: str) -> str:
-    return "".join(set(c for c in s if c in EMOJI_LIST))
+    return "".join(uniques(list(c for c in graphemes(s) if c in EMOJI_LIST)))
