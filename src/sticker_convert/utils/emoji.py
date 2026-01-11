617cb32e9f490843888bb9442e0951b314c522b3
laggykiller <chaudominic2@gmail.com>
2026-01-12 01:33:40 +0800

    Use pyuegc instead of ugrapheme for ease of compile; Fix unable to launch program

--- src/sticker_convert/utils/emoji.py
+++ src/sticker_convert/utils/emoji.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-from typing import Callable, List, cast
+from typing import List, cast
 
-from ugrapheme import grapheme_split  # type: ignore
+from pyuegc import EGC  # type: ignore
 
 from sticker_convert.definitions import RUNTIME_STATE
 from sticker_convert.utils.files.json_resources_loader import load_resource_json
-
-grapheme_split: Callable[[str], List[str]]  # type: ignore
 
 
 def get_emoji_list() -> List[str]:
@@ -24,7 +22,7 @@ EMOJI_LIST = get_emoji_list()
 # https://stackoverflow.com/a/43146653
 def extract_emojis(s: str) -> str:
     emojis: List[str] = []
-    for c in grapheme_split(s):
+    for c in cast(List[str], EGC(s)):
         if c in EMOJI_LIST and c not in emojis:
             emojis.append(c)
 
